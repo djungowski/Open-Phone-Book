01de7c37887f7415ebf6114f5d189fa820324666
Dominik Jungowski <Dominik@Jungowski.de>
2012-03-16 20:21:23 +0100

    Route /

--- src/OpenPhoneBook/WebBundle/Controller/DefaultController.php
+++ src/OpenPhoneBook/WebBundle/Controller/DefaultController.php
@@ -3,13 +3,17 @@
 namespace OpenPhoneBook\WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-    
-    public function indexAction($name)
+
+    /**
+     * @Template()
+     */
+    public function indexAction()
     {
-        return $this->render('OpenPhoneBookWebBundle:Default:index.html.twig', array('name' => $name));
+        return array();
+//        return $this->render('OpenPhoneBookWebBundle:Default:index.html.twig', array('name' => $name));
     }
 }
