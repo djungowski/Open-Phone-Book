e8a18ada5d1dec16395742149403fc137724b07f
Dominik Jungowski <Dominik@Jungowski.de>
2012-03-16 20:51:05 +0100

    Basic navigation

--- src/OpenPhoneBook/WebBundle/Resources/public/js/app/OpenPhoneBook/panel/Navigation.js
+++ src/OpenPhoneBook/WebBundle/Resources/public/js/app/OpenPhoneBook/panel/Navigation.js
@@ -1,6 +1,40 @@
 Ext.define('OpenPhoneBook.panel.Navigation', {
+    
     extend: 'Ext.panel.Panel',
+    
+    title: 'Navigation',
+        
     width: 250,
+    
     collapsible: true,
-    collapsed: true
+    
+    collapsed: true,
+    
+    initComponent: function() {
+        var menu,
+            menuItemLogin,
+            menuItemList;
+        
+        this.items = this.items || [];
+        
+        menuItemLogin = Ext.create('Ext.menu.Item', {
+            text: 'Einloggen'
+        });
+        
+        menuItemList = Ext.create('Ext.menu.Item', {
+            text: 'Telefonbuch anzeigen'
+        });
+        
+        menu = Ext.create('Ext.menu.Menu', {
+            floating: false,
+            items: [
+                menuItemLogin,
+                menuItemList
+            ]
+        });
+        
+        this.items.push(menu);
+        
+        this.callParent();
+    }
 });
