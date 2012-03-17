d565d4cf3259816cac6c961b8fa4f6c9bad7a662
Dominik Jungowski <Dominik@Jungowski.de>
2012-03-17 13:13:06 +0100

    Company

---
+++ src/OpenPhoneBook/WebBundle/Entity/Company.php
@@ -0,0 +1,56 @@
+<?php
+namespace OpenPhoneBook\WebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="company")
+ *
+ */
+class Company
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * 
+     */
+    protected $id;
+    
+    /**
+     * @ORM\Column(type="string")
+     * 
+     */
+    protected $name;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName( $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
